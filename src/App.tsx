6a2980ec924a57c97cd92be96527d3af429fb5ab
MaximeMG8 <max.mttl15@gmail.com>
2026-02-11 20:15:33 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,48 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Settings from "./pages/Settings";
+import Portfolio from "./pages/Portfolio";
 import ProjectDashboard from "./pages/ProjectDashboard";
 import MasterAnalytics from "./pages/MasterAnalytics";
 import NotFound from "./pages/NotFound";
+import { useProjectManager } from "@/hooks/useProjectManager";
 
 const queryClient = new QueryClient();
+
+const AppRoutes = () => {
+  const {
+    projects,
+    activeProjectId,
+    switchProject,
+    createProject,
+    deleteProject,
+    updateProject
+  } = useProjectManager();
+
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route
+        path="/portfolio"
+        element={
+          <Portfolio
+            projects={projects}
+            activeProjectId={activeProjectId}
+            onSwitchProject={switchProject}
+            onCreateProject={createProject}
+            onDeleteProject={deleteProject}
+            onUpdateProject={updateProject}
+          />
+        }
+      />
+      <Route path="/dashboard/:projectId" element={<ProjectDashboard />} />
+      <Route path="/master-analytics" element={<MasterAnalytics />} />
+      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -17,14 +54,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/dashboard/:projectId" element={<ProjectDashboard />} />
-          <Route path="/master-analytics" element={<MasterAnalytics />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
